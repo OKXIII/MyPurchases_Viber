8e987cfb94453d33844723b435f3ed23843d62f1
Konstantin <kovchinnikov@gmail.com>
2019-01-12 03:45:37 +0300

    1

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ from viberbot.api.bot_configuration import BotConfiguration
 bot_configuration = BotConfiguration(
 	name='MyPurchases',
 	avatar='https://github.com/OKXIII/MyPurchasesBot/blob/master/MyPurchasesBot.jpg?raw=true',
-	auth_token='47a71cffbce7d0eb-bb788620e6861ddf-7df845e6e386c7f4'
+#	auth_token='47a71cffbce7d0eb-bb788620e6861ddf-7df845e6e386c7f4'
+	auth_token='490fdf7a8567d79f-55497f0c7abb8e34-c86174030e8599c0'
 )
 viber = Api(bot_configuration)
--- server.py
+++ server.py
@@ -24,7 +24,7 @@ app = Flask(__name__)
 viber = Api(BotConfiguration(
   name='MyPurchases',
   avatar='https://github.com/OKXIII/MyPurchasesBot/blob/master/MyPurchasesBot.jpg?raw=true',
-  auth_token='47a71cffbce7d0eb-bb788620e6861ddf-7df845e6e386c7f4'
+  auth_token='490fdf7a8567d79f-55497f0c7abb8e34-c86174030e8599c0'
 ))
 
 #@app.route('/', methods=['POST'])
