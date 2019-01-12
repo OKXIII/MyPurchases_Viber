7f27637d398d726cedcf913dfa3980a666d4dada
Konstantin <kovchinnikov@gmail.com>
2019-01-12 16:54:31 +0300

    1

--- server.py
+++ server.py
@@ -67,10 +67,10 @@ def set_webhook(viber):
 
 
 if __name__ == "__main__":
-	scheduler = sched.scheduler(time.time, time.sleep)
-	scheduler.enter(5, 1, set_webhook, (viber,))
-	t = threading.Thread(target=scheduler.run)
-	t.start()
+#	scheduler = sched.scheduler(time.time, time.sleep)
+#	scheduler.enter(5, 1, set_webhook, (viber,))
+#	t = threading.Thread(target=scheduler.run)
+#	t.start()
 
 	context = ('server.crt', 'server.key')
 	app.run(host='0.0.0.0', port=443, debug=True, ssl_context=context)
